c91756cbb2b01a85ab931f389f8b86d7769fc2f1
Yassyn06 <labonnesource7@gmail.com>
2024-01-18 10:40:41 +0100

    deuxième commit

--- Test_Orange_HRM/test_ajout_supprimer.py
+++ Test_Orange_HRM/test_ajout_supprimer.py
@@ -6,7 +6,6 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
-
 class TestCaseAjouterSupprimer(unittest.TestCase):
 
     def setUp(self):
@@ -19,8 +18,9 @@ class TestCaseAjouterSupprimer(unittest.TestCase):
     def test_case_ajouter_supprimer(self):
         driver = self.driver
        # Connexion
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        
+        driver.get(
+            "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
         driver.find_element(By.NAME, "username").click()
         driver.find_element(By.NAME, "username").clear()
         driver.find_element(By.NAME, "username").send_keys("Admin")
@@ -31,17 +31,20 @@ class TestCaseAjouterSupprimer(unittest.TestCase):
         driver.find_element(By.XPATH, "//button[@type='submit']").click()
         time.sleep(0.5)
 
-        
         # Navigation sur la page viewEmployeeList
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
+        driver.get(
+            "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
         driver.find_element(By.LINK_TEXT, "PIM").click()
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList")
+        driver.get(
+            "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList")
         time.sleep(0.5)
 
        # Ajout de l'élément:
-        
-        driver.find_element(By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div[2]/div/button").click()
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmployee")
+
+        driver.find_element(
+            By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div[2]/div/button").click()
+        driver.get(
+            "https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmployee")
 
         driver.find_element(By.NAME, "firstName").click()
         driver.find_element(By.NAME, "firstName").clear()
@@ -51,62 +54,48 @@ class TestCaseAjouterSupprimer(unittest.TestCase):
         driver.find_element(By.NAME, "lastName").send_keys("world")
         time.sleep(0.5)
         driver.find_element(By.XPATH, "//button[@type='submit']").click()
-        
+
         time.sleep(3)
 
-
        # Suppression de l'élément:
-        
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/110")
-        driver.find_element(By.XPATH, "//div[@id='app']/div/div/aside/nav/div[2]/ul/li[2]/a/span").click()
-        
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList")
-        driver.find_element(By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/form/div/div/div/div/div[2]/div/div/input").click()
-        driver.find_element(By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/form/div/div/div/div/div[2]/div/div/input").clear()
-        driver.find_element(By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/form/div/div/div/div/div[2]/div/div/input").send_keys("hello  world")
+
+        driver.get(
+            "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/110")
+        driver.find_element(
+            By.XPATH, "//div[@id='app']/div/div/aside/nav/div[2]/ul/li[2]/a/span").click()
+
+        driver.get(
+            "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList")
+        driver.find_element(
+            By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/form/div/div/div/div/div[2]/div/div/input").click()
+        driver.find_element(
+            By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/form/div/div/div/div/div[2]/div/div/input").clear()
+        driver.find_element(
+            By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/form/div/div/div/div/div[2]/div/div/input").send_keys("hello  world")
         time.sleep(0.5)
         driver.find_element(By.XPATH, "//button[@type='submit']").click()
-        
-        
+
         nombre_elements = driver.find_elements(By.CLASS_NAME, "oxd-table-card")
         print(f'Nombre des éléments après ajout : {len(nombre_elements)}')
         time.sleep(0.5)
 
-        driver.find_element(By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div/div/div/label/span/i").click()
-        driver.find_element(By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[9]/div/button/i").click()
-        driver.find_element(By.XPATH, "//div[@id='app']/div[3]/div/div/div/div[3]/button[2]").click()
+        driver.find_element(
+            By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div/div/div/label/span/i").click()
+        driver.find_element(
+            By.XPATH, "//div[@id='app']/div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[9]/div/button/i").click()
+        driver.find_element(
+            By.XPATH, "//div[@id='app']/div[3]/div/div/div/div[3]/button[2]").click()
         time.sleep(0.5)
 
         nombre_elements = driver.find_elements(By.CLASS_NAME, "oxd-table-card")
         print(f'Nombre des éléments après suppression : {len(nombre_elements)}')
         time.sleep(3)
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
 
+
 if __name__ == "__main__":
     unittest.main()
-
-    
